96776efe3b9214ee054e9673f8ada7cbeffa8670
rafiamastoor <rafiamastoor@gmail.com>
2025-08-12 14:59:54 +0100

    development-rafia1

---
+++ Server/models/blog.js
@@ -0,0 +1,51 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Blog extends Model {}
+
+Blog.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    blog_title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    blog_content: {
+      type: DataTypes.TEXT("long"),
+      allowNull: false,
+    },
+    thumbnail_image: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "user",
+        key: "id",
+      },
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "category",
+        key: "id",
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "blog",
+  }
+);
+
+module.exports = Blog;
